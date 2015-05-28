0a026df5a9c74218f4c9b55d591c02e8a3e5e586
月笙 <xing.gex@alibaba-inc.com>
2015-05-28 09:38:28 +0800

    Fix #5,add Object.create polyfill.

--- src/lexer.js
+++ src/lexer.js
@@ -98,6 +98,33 @@ var parseGet = function (object, path) {
     return val;
 };
 
+
+if (typeof Object.create != 'function') {
+    Object.create = (function() {
+        function Temp() {}
+        var hasOwn = Object.prototype.hasOwnProperty;
+        return function (O) {
+            if (typeof O != 'object') {
+                throw TypeError('Object prototype may only be an Object or null');
+            }
+
+            Temp.prototype = O;
+            var obj = new Temp();
+            Temp.prototype = null;
+
+            if (arguments.length > 1) {
+                var Properties = Object(arguments[1]);
+                for (var prop in Properties) {
+                    if (hasOwn.call(Properties, prop)) {
+                        obj[prop] = Properties[prop];
+                    }
+                }
+            }
+            return obj;
+        };
+    })();
+}
+
 var parser = (function () {
     var o = function (k, v, o, l) {
         for (o = o || {}, l = k.length; l--; o[k[l]] = v);
